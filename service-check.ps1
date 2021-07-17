151e5aa2bab151bf2decd8d5ba356ce6ef4c84c2
bluesDD <dd11blues@gmail.com>
2021-07-18 03:10:41 +0900

    1st commit

---
+++ service-check.ps1
@@ -0,0 +1,12 @@
+﻿Write-Output "Start checking services !"
+
+# 確認したいサービス名の配列
+$SERVICES = @("SENS", "seclogon")
+
+# foreach ($service In $SERVICES) {
+#     Get-Service　$service
+# }
+
+Get-Service　$SERVICES
+
+Write-Output "Script ended 1 !"
