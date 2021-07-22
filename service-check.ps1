293ffd87c8b8bb42aceaf5f03c26ddaeda26d16a
bluesDD <dd11blues@gmail.com>
2021-07-23 03:39:15 +0900

    popupmessage

--- service-check.ps1
+++ service-check.ps1
@@ -4,14 +4,21 @@
 $SERVICES = @("SENS", "seclogon")
 $SERVICE_HEALTH = "Running"
 
-$services_status =　Get-Service　$SERVICES |ForEach-Object{$_.Status}
+$services_result =　Get-Service　$SERVICES
+$services_status =　$services_result |ForEach-Object{$_.Status}
+$services_name =　$services_result |ForEach-Object{$_.Name}
 $exit_message = "All target services is runnning!" 
 
 for ($i = 0; $i -lt $services_status.Count; $i++) {
     if ($services_status[$i] -ne $SERVICE_HEALTH) {
-        $exit_message = "Some service are not running..."
+        $exit_message = "Some services are not running..."
+        Write-Output "$services_name[$i] is not working"
     }
 }
-
-Write-Output $exit_message
+Show-Message $exit_message
 Write-Output "Finished checking services."
+
+function Show-Message ($message){
+    $obj = New-Object -ComObject wscript.shell
+    return $obj.popup($message)
+}
